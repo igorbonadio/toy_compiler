b1236ae6014557a7c8fa09d1f240f290472e27f1
Igor Bonadio <igorbonadio@gmail.com>
2014-08-05 13:57:48 -0300

    Maven can build a dist zip

--- src/main/java/br/com/igorbonadio/likely/lklc/App.java
+++ src/main/java/br/com/igorbonadio/likely/lklc/App.java
@@ -1,13 +1,19 @@
 package br.com.igorbonadio.likely.lklc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class App {
+  public static void main( String[] args ) {
+    try {
+      ANTLRInputStream input = new ANTLRInputStream(System.in);
+      LikelyLexer lexer = new LikelyLexer(input);
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      LikelyParser parser = new LikelyParser(tokens);
+      ParseTree tree = parser.file_input();
+      System.out.println(tree.toStringTree(parser));
+    } catch(Exception e) {
+      System.out.println("error...");
     }
+  }
 }
